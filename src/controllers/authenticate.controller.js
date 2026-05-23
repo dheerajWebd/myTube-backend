a590f04724a0866f692248519e3d4b0557d511ff
dheeraj kumar dwivedi <dheerajdwivediweb@gmail.com>
2026-05-23 13:50:28 +0530

    fix: correct typo in authentication error message and improve formatting

--- src/controllers/authenticate.controller.js
+++ src/controllers/authenticate.controller.js
@@ -1,9 +1,10 @@
 import asyncHandler from "../utils/ansicHandler.js";
+import { ErrorFormater } from "../utils/ErrorFormate.js";
 import successResponse from "../utils/successResponse.js";
 
 const authenticte = asyncHandler(async (req, res, next) => {
  const user = req.user
-
+ 
  if (!user) {
   throw new ErrorFormater("unathorised requested plz login", "", 401);
  }
