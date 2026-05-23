174350d5309f566f307ffcee1d1b1da3df1f5853
dheeraj kumar dwivedi <dheerajdwivediweb@gmail.com>
2026-05-23 19:20:57 +0530

    feat: add avatar and cover image files for user profiles

--- src/controllers/authenticate.controller.js
+++ src/controllers/authenticate.controller.js
@@ -3,13 +3,12 @@ import { ErrorFormater } from "../utils/ErrorFormate.js";
 import successResponse from "../utils/successResponse.js";
 
 const authenticte = asyncHandler(async (req, res, next) => {
- const user = req.user
- 
- if (!user) {
-  throw new ErrorFormater("unathorised requested plz login", "", 401);
- }
+  const user = req.user;
+
+  if (!user) {
+    throw new ErrorFormater("unathorised requested plz login", "", 401);
+  }
 
   res.status(200).json(new successResponse(200, user, "user authenticated"));
- 
 });
 export default authenticte;
