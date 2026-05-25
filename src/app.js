e40b9496c3c3296aadc3eff47c49ffa658fa92b8
dheeraj kumar dwivedi <dheerajdwivediweb@gmail.com>
2026-05-25 21:00:11 +0530

    deplomint setting

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: LIMITE_DATA }));
 app.use(
   cors({
-    origin: [process.env.ORIGIN_FRONTEND, "http://localhost:3000"],
+    origin: [process.env.ORIGIN_FRONTEND, "http://localhost:3000","https://mytubedheeraj.netlify.app/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
